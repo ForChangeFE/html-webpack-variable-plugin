bdb6cb82da6eec37ad3dfc5e5d4c446ac7965397
zhazheng <512557403@qq.com>
2018-12-09 18:38:15 +0800

    feat: release project with version-1.0.0

---
+++ index.js
@@ -0,0 +1,25 @@
+function VariablePlugin(options) {
+    var varsMap = {};
+    try {
+        // should pass json with options.map
+        var map = options.map || "";
+        varsMap = JSON.parse(map);
+    }
+    catch (error) {
+        throw new TypeError("[html-webpack-variable-plugin]: Option is invalid. Should pass json with options.map.");
+    }
+    var varsStr = Object.keys(varsMap).reduce(function (accumulator, key) {
+        return (accumulator += key + "='" + varsMap[key] + "';");
+    }, "");
+    this.script = varsStr;
+}
+VariablePlugin.prototype.apply = function (compiler) {
+    var self = this;
+    compiler.plugin("compilation", function (compilation) {
+        compilation.plugin("html-webpack-plugin-before-html-processing", function (htmlPluginData, callback) {
+            htmlPluginData.html = htmlPluginData.html.replace("<head>", "<head><script>" + self.script + "</script>");
+            callback(null, htmlPluginData);
+        });
+    });
+};
+module.exports = VariablePlugin;
---
+++ index.ts
@@ -0,0 +1,45 @@
+/**
+ * @author: zhazheng
+ * @since: 2018/11/18
+ * inject variables in process.env to window/global
+ */
+interface Options {
+  map?: string;
+}
+function VariablePlugin(options: Options) {
+  let varsMap = {};
+
+  try {
+    // should pass json with options.map
+    const map = options.map || "";
+    varsMap = JSON.parse(map);
+  } catch (error) {
+    throw new TypeError(
+      `[html-webpack-variable-plugin]: Option is invalid. Should pass json with options.map.`
+    );
+  }
+
+  const varsStr = Object.keys(varsMap).reduce((accumulator, key) => {
+    return (accumulator += `${key}='${varsMap[key]}';`);
+  }, "");
+
+  this.script = varsStr;
+}
+
+VariablePlugin.prototype.apply = function(compiler) {
+  const self = this;
+  compiler.plugin("compilation", function(compilation) {
+    compilation.plugin("html-webpack-plugin-before-html-processing", function(
+      htmlPluginData,
+      callback
+    ) {
+      htmlPluginData.html = htmlPluginData.html.replace(
+        "<head>",
+        `<head><script>${self.script}</script>`
+      );
+      callback(null, htmlPluginData);
+    });
+  });
+};
+
+module.exports = VariablePlugin;
